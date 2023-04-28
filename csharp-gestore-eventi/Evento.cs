c55ec85f2711e5e493c8282c15a7fb112192d2ac
JennyPersichini <nenny1096@gmail.com>
2023-04-28 10:38:03 +0200

    Creata Evento class and init

---
+++ csharp-gestore-eventi/Evento.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_gestore_eventi
+{
+    public class Evento
+    {
+        //ATTRIBUTI
+        private string titolo;
+        private DateTime data;
+        private int capienzaMassima;
+        private int postiPrenotati;
+
+        //COSTRUTTORE
+        public Evento(string titolo, string dataStringa, int capienzaMassima)
+        {
+            this.titolo = titolo;
+            this.data = dataStringa;
+            this.capienzaMassima = capienzaMassima;
+            this.postiPrenotati = 0;
+        }
+
+        //GETTERS
+        public string GetTitolo()
+        {
+            return this.titolo;
+        }
+        public DateTime GetData()
+        {
+            return this.data;
+        }
+        public int GetCapienzaMassima()
+        {
+            return this.capienzaMassima;
+        }
+        public int GetPostiPrenotati()
+        {
+            return this.postiPrenotati;
+        }
+
+        //SETTERS
+        
+
+
+    }
+}
