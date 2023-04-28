228a9b13ab330bf9e8da6342ba46f2be33299283
JennyPersichini <nenny1096@gmail.com>
2023-04-28 15:01:18 +0200

    Add 2 methods

--- csharp-gestore-eventi/ProgrammaEventi.cs
+++ csharp-gestore-eventi/ProgrammaEventi.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Metadata;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,5 +14,56 @@ namespace csharp_gestore_eventi
         //ATTRIBUTI
         private string titolo;
         private List<Evento> eventi;
+
+        //COSTRUTTORE
+        public ProgrammaEventi(string titolo)
+        {
+            this.titolo = titolo;
+            this.eventi = new List<Evento>();
+        }
+
+        //GETTER
+        public string GetTitolo()
+        {
+            return this.titolo;
+        }
+
+        //METODI
+
+        //1. un metodo che aggiunge alla lista del programma eventi un Evento, passato come parametro al metodo
+        public void AggiungiEvento(Evento evento)
+        {
+            this.eventi.Add(evento);
+        }
+
+        //2. un metodo che restituisce una lista di eventi con tutti gli eventi presenti in una certa data
+        public List<Evento> EventiInData(string dataString)
+        {
+            DateTime data = DateTime.Parse(dataString);
+
+            List<Evento> ListaEventiInData = new List<Evento>();
+
+            foreach (Evento evento in this.eventi)
+            {
+                if (evento.GetData() == data)
+                {
+                    ListaEventiInData.Add(evento);
+                }
+            }
+            return ListaEventiInData;
+        }
+
+        //3. un metodo statico che si occupa, presa una lista di eventi, di stamparla in Console,
+        //o restituisca la rappresentazione in stringa della lista di eventi
+
+
+        //4. un metodo che restituisce quanti eventi sono presenti attualmente
+
+
+        //5. un metodo che svuota la lista di eventi
+
+
+        //6. un metodo che restituisce una stringa che mostra il titolo del programma e tutti gli eventi aggiunti alla lista
+        
     }
 }
