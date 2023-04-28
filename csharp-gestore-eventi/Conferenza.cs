297a8c182b4a02a06c4cba68960f6a37dcb7fd04
JennyPersichini <nenny1096@gmail.com>
2023-04-28 17:47:07 +0200

    init class Conferenza (bonus)

---
+++ csharp-gestore-eventi/Conferenza.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+//BONUS
+
+namespace csharp_gestore_eventi
+{
+    public class Conferenza : Evento
+    {
+        //ATTRIBUTI
+        private string relatore;
+        private double prezzo;
+
+        //COSTRUTTORE
+        public Conferenza(string titolo, string relatore, string dataStringa, int capienzaMassima, double prezzo) : base(titolo, dataStringa, capienzaMassima)
+        {
+            this.relatore = relatore;
+            this.prezzo = prezzo;
+        }
+
+        //GETTERS
+        public string GetRelatore()
+        {
+            return this.relatore;
+        }
+
+        public double GetPrezzo()
+        {
+            return this.prezzo;
+        }
+
+        //SETTERS
+        public void SetRelatore(string relatore)
+        {
+            this.relatore = relatore;
+        }
+
+        public void SetPrezzo(double prezzo)
+        {
+            this.prezzo = prezzo;
+        }
+    }
+}
