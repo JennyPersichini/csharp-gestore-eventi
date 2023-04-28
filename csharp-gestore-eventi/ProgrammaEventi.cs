a3148e8d847fcb6264b8cae0cd59c3b4d1ba0689
JennyPersichini <nenny1096@gmail.com>
2023-04-28 14:15:58 +0200

    Add class ProgrammaEventi with attributi

---
+++ csharp-gestore-eventi/ProgrammaEventi.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_gestore_eventi
+{
+    public class ProgrammaEventi
+    {
+        //ATTRIBUTI
+        private string titolo;
+        private List<Evento> eventi;
+    }
+}
